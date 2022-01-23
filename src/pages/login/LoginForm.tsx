4a4a693dbb0513500c889322480ae62af6c0382b
yunhwan <jimmy53120488@gmail.com>
2022-01-23 19:52:02 +0900

    Feat: LoginForm 컴포넌트의 제어 컴포넌트화
    - single source of truth인 react state에 input의 state를 결합하여 관리하도록 적용.
    - input에 type, name 속성 추가.

--- src/pages/login/LoginForm.tsx
+++ src/pages/login/LoginForm.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, Link, TextField, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 function LoginForm() {
+    const [emailState, setEmailState] = useState('');
+    const [passwordState, setPasswordState] = useState('');
+
+    function updateEmailState(e: React.ChangeEvent<HTMLInputElement>) {
+        setEmailState(e.target.value);
+    }
+
+    function updatePasswordState(e: React.ChangeEvent<HTMLInputElement>) {
+        setPasswordState(e.target.value);
+    }
+
     return (
         <Box component="form" sx={{ mt: 1 }}>
-            <TextField margin="normal" fullWidth required label="Email" />
-            <TextField margin="normal" fullWidth required label="Password" />
+            <TextField
+                margin="normal"
+                fullWidth
+                required
+                type="email"
+                name="email"
+                label="Email"
+                value={emailState}
+                onChange={updateEmailState}
+            />
+            <TextField
+                margin="normal"
+                fullWidth
+                required
+                type="password"
+                name="password"
+                label="Password"
+                value={passwordState}
+                onChange={updatePasswordState}
+            />
             <Button fullWidth variant="contained" type="submit" sx={{ mt: 3 }}>
                 로그인
             </Button>
