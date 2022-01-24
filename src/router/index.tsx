3ecf59bcd0deec95fa2c14c1f67c36e1d4d1641f
yunhwan <jimmy53120488@gmail.com>
2022-01-25 06:35:41 +0900

    Refactor: 라우트 기반 코드 스플리팅 적용

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,37 +1,41 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import LoginPage from '../pages/login';
 import { useLoginState } from '../context/global/LoginContext';
-import UserPage from '../pages/user';
-import ResetPasswordPage from '../pages/reset-password';
+import { LinearProgress } from '@mui/material';
+
+const UserPage = lazy(() => import('../pages/user'));
+const LoginPage = lazy(() => import('../pages/login'));
+const ResetPasswordPage = lazy(() => import('../pages/reset-password'));
 
 function Router() {
     const isLoggedIn = !!useLoginState();
     return (
         <BrowserRouter>
-            <Routes>
-                {isLoggedIn ? (
-                    <>
-                        <Route path="/user" element={<UserPage />} />
-                        <Route
-                            path="*"
-                            element={<Navigate replace to={'/user'} />}
-                        />
-                    </>
-                ) : (
-                    <>
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route
-                            path="/reset-password"
-                            element={<ResetPasswordPage />}
-                        />
-                        <Route
-                            path="*"
-                            element={<Navigate replace to="login" />}
-                        />
-                    </>
-                )}
-            </Routes>
+            <Suspense fallback={<LinearProgress />}>
+                <Routes>
+                    {isLoggedIn ? (
+                        <>
+                            <Route path="/user" element={<UserPage />} />
+                            <Route
+                                path="*"
+                                element={<Navigate replace to={'/user'} />}
+                            />
+                        </>
+                    ) : (
+                        <>
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route
+                                path="/reset-password"
+                                element={<ResetPasswordPage />}
+                            />
+                            <Route
+                                path="*"
+                                element={<Navigate replace to="login" />}
+                            />
+                        </>
+                    )}
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     );
 }
