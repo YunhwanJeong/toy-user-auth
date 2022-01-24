3a07f47a770773701810b204694d4daa45514807
yunhwan <jimmy53120488@gmail.com>
2022-01-25 06:52:22 +0900

    Refactor: 서버 정상 응답 외에는 Axios interceptor에서 알 수 없는 에러로 한 번에 응답하도록 적용

--- src/utils/CustomAxios.ts
+++ src/utils/CustomAxios.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const DEFAULT_BASE_URL = 'https://ably-frontend-assignment-server.vercel.app';
 const DEFAULT_HEADERS = { 'Content-Type': 'application/json' };
+const UNKNOWN_ERROR_MESSAGE = '알 수 없는 에러가 발생하였습니다!!!';
 
 export interface AxiosErrorResponseData {
     error: {
@@ -13,5 +14,24 @@ const customAxios = axios.create({
     baseURL: DEFAULT_BASE_URL,
     headers: DEFAULT_HEADERS,
 });
+
+customAxios.interceptors.response.use(
+    function (config) {
+        return config;
+    },
+    function (error) {
+        if (!error.response) {
+            error.response = {
+                data: {
+                    error: {
+                        message: UNKNOWN_ERROR_MESSAGE,
+                    },
+                },
+            };
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default customAxios;
