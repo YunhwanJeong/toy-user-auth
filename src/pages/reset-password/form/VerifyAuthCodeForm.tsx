085d2f06317809bb250784c3d9062e366810c954
yunhwan <jimmy53120488@gmail.com>
2022-01-25 01:45:11 +0900

    Design: 인증 코드 입력 화면 마크업

--- src/pages/reset-password/form/VerifyAuthCodeForm.tsx
+++ src/pages/reset-password/form/VerifyAuthCodeForm.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Button, InputAdornment, TextField } from '@mui/material';
 
 function VerifyAuthCodeForm() {
-    return <div></div>;
+    const [authCode, setAuthCode] = useState('');
+
+    function updateAuthCode(e: React.ChangeEvent<HTMLInputElement>) {
+        setAuthCode(e.target.value);
+    }
+
+    return (
+        <Box component="form" sx={{ mt: 10 }}>
+            <TextField
+                fullWidth
+                required
+                label="인증코드"
+                value={authCode}
+                onChange={updateAuthCode}
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment disablePointerEvents position="end">
+                            mm:ss
+                        </InputAdornment>
+                    ),
+                }}
+            />
+            <Box
+                sx={{
+                    mt: 5,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    gap: 1,
+                }}
+            >
+                <Button fullWidth size="large" variant="outlined">
+                    이전
+                </Button>
+                <Button fullWidth variant="contained" type="submit">
+                    다음
+                </Button>
+            </Box>
+        </Box>
+    );
 }
 
 export default VerifyAuthCodeForm;
